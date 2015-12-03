c96767ac38743b708e007e143af69184f05476b9
Michal Talaga <mikeon@objectware.pl>
2015-12-03 14:47:11 +0100

    initial

---
+++ ToastmastersDashboardDownloader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToastmastersDashboardDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"http://dashboards.toastmasters.org/export.aspx?type=CSV&report=districtperformance~95~11/30/2015~~2015-2016";
+        }
+    }
+}
